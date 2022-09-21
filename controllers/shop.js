777362d42f2cec412e9218517ba18cb8756a894f
TarasSydorovych <jakzadarom2@gmail.com>
2022-09-21 14:50:06 +0300

    dfdf

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,6 +4,7 @@ const url = 'mongodb://0.0.0.0:27017/'
 const shopsController = new Router();
 
 shopsController.get('/shop1', function (req, res) {
+
     MongoClient.connect(url, function (err, db) {
         if (err) throw res.sendStatus(500);
         let dbo = db.db('shop')
--- controllers/startPage.js
+++ controllers/startPage.js
@@ -4,6 +4,7 @@ const url = 'mongodb://0.0.0.0:27017/'
 const startPage = new Router();
 
 startPage.get('/', function (req, res) {
+
     MongoClient.connect(url, function (err, db) {
         if (err) throw res.sendStatus(500);
         let dbo = db.db('shop')
